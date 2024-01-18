e8cccd13f74dcc1036451c6d939b7bd9e3849127
Mohamad Jouni <33843331+mhmdjouni@users.noreply.github.com>
2024-01-18 15:00:52 +0100

    Basic direct model environment

---
+++ src/demo/demo_direct_model.py
@@ -0,0 +1,73 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.common_utils.custom_vars import InterferometerType as IfmType
+from src.direct_model.interferometer import interferometer_factory
+from src.common_utils.light_wave import Spectrum
+
+
+def main():
+    """
+    For a future slider, my variables are:
+    - Reflectance: To increase N as the N-wave approximation in FP with order 0
+    - OPD index in the plot of the transfer matrices: To observe the OPD responses and their DCT
+    - OPD value in the cosines that constitute the spectral radiance: Taken from the list of OPDs for controlled observation of the interferograms
+    """
+    opds = np.linspace(start=0, stop=56, num=320, endpoint=False)
+    wavenumbers = np.linspace(start=0, stop=1/(2*np.mean(np.diff(opds))), num=opds.size*6, endpoint=False)
+    reflectance = 0.13 * np.ones_like(a=wavenumbers, dtype=np.float_)
+    transmittance = 1 - reflectance
+
+    plot_opd_idx = 300
+    radiance_cosine_args = {
+        "amplitudes": np.array([1, 1]),
+        "opds": opds[[50, 300]],
+    }
+
+    michelson = interferometer_factory(option=IfmType.MICHELSON, transmittance=transmittance, opds=opds)
+    fabry_perot = interferometer_factory(option=IfmType.FABRY_PEROT, transmittance=transmittance, opds=opds)
+    fabry_perot_2 = interferometer_factory(option=IfmType.FABRY_PEROT, transmittance=transmittance, opds=opds, order=2)
+
+    transfer_matrix_mich = michelson.generate_transmittance_response(wavenumbers=wavenumbers)
+    transfer_matrix_fp = fabry_perot.generate_transmittance_response(wavenumbers=wavenumbers)
+    transfer_matrix_fp_2 = fabry_perot_2.generate_transmittance_response(wavenumbers=wavenumbers)
+
+    spectrum = Spectrum.from_oscillations(
+        amplitudes=radiance_cosine_args["amplitudes"],
+        opds=radiance_cosine_args["opds"],
+        wavenumbers=wavenumbers,
+    )
+
+    interferogram_mich = michelson.acquire_interferogram(spectrum=spectrum)
+    interferogram_fp = fabry_perot.acquire_interferogram(spectrum=spectrum)
+    interferogram_fp_2 = fabry_perot_2.acquire_interferogram(spectrum=spectrum)
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, squeeze=False)
+    transfer_matrix_mich.visualize(axs=axs[0, 0])
+    transfer_matrix_mich.visualize_opd_response(axs=axs[0, 1], opd_idx=plot_opd_idx)
+    transfer_matrix_mich.visualize_dct(axs=axs[1, 0], opd_idx=-1)
+    transfer_matrix_mich.visualize_singular_values(axs=axs[1, 1])
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, squeeze=False)
+    transfer_matrix_fp_2.visualize(axs=axs[0, 0])
+    transfer_matrix_fp_2.visualize_opd_response(axs=axs[0, 1], opd_idx=plot_opd_idx)
+    transfer_matrix_fp_2.visualize_dct(axs=axs[1, 0], opd_idx=plot_opd_idx)
+    transfer_matrix_fp_2.visualize_singular_values(axs=axs[1, 1])
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, squeeze=False)
+    transfer_matrix_fp.visualize(axs=axs[0, 0])
+    transfer_matrix_fp.visualize_opd_response(axs=axs[0, 1], opd_idx=plot_opd_idx)
+    transfer_matrix_fp.visualize_dct(axs=axs[1, 0], opd_idx=plot_opd_idx)
+    transfer_matrix_fp.visualize_singular_values(axs=axs[1, 1])
+
+    fig, axs = plt.subplots(nrows=2, ncols=2, squeeze=False)
+    spectrum.visualize(axs=axs[0, 0])
+    interferogram_mich.visualize(axs=axs[0, 1])
+    interferogram_fp.visualize(axs=axs[1, 0])
+    interferogram_fp_2.visualize(axs=axs[1, 1])
+
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/direct_model/interferometer.py
@@ -0,0 +1,104 @@
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+
+import numpy as np
+
+from src.common_utils.interferogram import Interferogram
+from src.common_utils.light_wave import Spectrum
+from src.common_utils.transmittance_response import TransmittanceResponse
+from src.common_utils.custom_vars import Wvn, Opd, InterferometerType
+
+
+@dataclass(frozen=True)
+class Interferometer(ABC):
+    transmittance: np.ndarray[tuple[Wvn], np.dtype[np.float_]]
+    opds: np.ndarray[tuple[Opd], np.dtype[np.float_]]
+
+    @abstractmethod
+    def generate_transmittance_response(
+            self,
+            wavenumbers: np.ndarray[tuple[Wvn], np.dtype[np.float_]],
+    ) -> TransmittanceResponse:
+        pass
+
+    def acquire_interferogram(
+            self,
+            spectrum: Spectrum,
+    ) -> Interferogram:
+        transmittance_response = self.generate_transmittance_response(wavenumbers=spectrum.wavenumbers)
+        interferogram = transmittance_response.data @ spectrum.data
+        return Interferogram(
+            data=interferogram,
+            opds=self.opds,
+        )
+
+
+@dataclass(frozen=True)
+class MichelsonInterferometer(Interferometer):
+
+    def generate_transmittance_response(
+            self,
+            wavenumbers: np.ndarray[tuple[Wvn], np.dtype[np.float_]],
+    ) -> TransmittanceResponse:
+        phase_difference = calculate_phase_difference(opds=self.opds, wavenumbers=wavenumbers)
+        transmittance_response = 2 * self.transmittance[None, :] * (1 + np.cos(2 * np.pi * phase_difference))
+        return TransmittanceResponse(
+            data=transmittance_response,
+            wavenumbers=wavenumbers,
+            opds=self.opds,
+        )
+
+
+@dataclass(frozen=True)
+class FabryPerotInterferometer(Interferometer):
+    order: int = 0
+
+    def generate_transmittance_response(
+            self,
+            wavenumbers: np.ndarray[tuple[Wvn], np.dtype[np.float_]],
+    ) -> TransmittanceResponse:
+        reflectance = 1 - self.transmittance
+        phase_difference = self.opds[:, None] * wavenumbers[None, :]
+
+        if self.order == 0:
+            # Using the infinity-wave model and geometric series formula
+            denominator = 1 + reflectance[None, :] ** 2 - 2 * reflectance[None, :] * np.cos(2 * np.pi * phase_difference)
+            transmittance_response = self.transmittance[None, :] ** 2 * (1 / denominator)
+
+        else:
+            # Using the N-wave model approximation and the Poisson kernel formula
+            q = 1 / (1 - reflectance ** 2)
+            n_values = np.arange(1, self.order)
+            reflectance_factors = reflectance[None, None, :] ** n_values[:, None, None]
+            cosine_factors = np.cos(2 * np.pi * n_values[:, None, None] * phase_difference[None, :])
+            series_sum = 1 + 2 * np.sum(reflectance_factors * cosine_factors, axis=0)
+            transmittance_response = self.transmittance[None, :] ** 2 * (q[None, :] * series_sum)
+
+        return TransmittanceResponse(
+            data=transmittance_response,
+            wavenumbers=wavenumbers,
+            opds=self.opds,
+        )
+
+
+def calculate_phase_difference(
+        opds: np.ndarray[tuple[Opd], np.dtype[np.float_]],
+        wavenumbers: np.ndarray[tuple[Wvn], np.dtype[np.float_]],
+) -> np.ndarray[tuple[Opd, Wvn], np.dtype[np.float_]]:
+    return opds[:, None] * wavenumbers[None, :]
+
+
+def interferometer_factory(
+        option: InterferometerType,
+        transmittance: np.ndarray[tuple[Wvn], np.dtype[np.float_]],
+        opds: np.ndarray[tuple[Opd], np.dtype[np.float_]],
+        order: int = 0,
+) -> Interferometer:
+    # TODO: Parameters to be optimized?
+    if option == InterferometerType.MICHELSON:
+        interferometer = MichelsonInterferometer(transmittance=transmittance, opds=opds)
+    elif option == InterferometerType.FABRY_PEROT:
+        interferometer = FabryPerotInterferometer(transmittance=transmittance, opds=opds, order=order)
+    else:
+        raise ValueError(f"Option '{option.value}' is not supported")
+    return interferometer
