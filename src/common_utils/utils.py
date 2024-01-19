c5f60172b3b32620b952b2775cb6f35746329293
Mohamad Jouni <33843331+mhmdjouni@users.noreply.github.com>
2024-01-19 18:01:32 +0100

    Minor modifications for demo_direct_model.py

--- src/common_utils/utils.py
+++ src/common_utils/utils.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from src.common_utils.custom_vars import Opd, Wvn
+
 
 def generate_shifted_dirac(array: np.ndarray, shift: float) -> np.ndarray:
     dirac_signal = np.zeros_like(array)
@@ -11,3 +13,21 @@ def generate_shifted_dirac(array: np.ndarray, shift: float) -> np.ndarray:
 def index_from_value(array: np.ndarray, value: float) -> int:
     index = (np.abs(array - value)).argmin()
     return index
+
+
+def generate_sampled_opds(nb_opd: int, del_opd: float):
+    opds = del_opd * np.arange(nb_opd)
+    return opds
+
+
+def generate_wavenumbers_from_opds(nb_wn: int, del_opd: float):
+    del_wn = 1 / (2 * nb_wn * del_opd)  # del_wn tends to zero as nb_wn tends to infinity (implies continuous)
+    wavenumbers = del_wn * (np.arange(nb_wn) + 1/2)
+    return wavenumbers
+
+
+def generate_wavenumbers_in_idct(
+        opds: np.ndarray[tuple[Opd], np.dtype[np.float_]],
+) -> np.ndarray[tuple[Wvn], np.dtype[np.float_]]:
+    wavenumbers = generate_wavenumbers_from_opds(nb_wn=opds.size, del_opd=np.mean(np.diff(opds)))
+    return wavenumbers
