64a7e189ff37383019529cc9555832a6eadbcaac
Mohamad Jouni <33843331+mhmdjouni@users.noreply.github.com>
2024-02-15 12:14:41 +0100

    Invert simulated data and save reconstruction

--- src/demo/experiments_paper/snr/invert.py
+++ src/demo/experiments_paper/snr/invert.py
@@ -1,7 +1,6 @@
 """
-Exhaustive working template for simulated inversion, with comments and extra functionality (saving arrays, etc.)
+Minimal working template for simulated inversion, without comments and extra functionality (saving arrays, etc.)
 """
-
 from pprint import pprint
 
 import numpy as np
@@ -9,110 +8,78 @@ import numpy as np
 from src.common_utils.utils import calculate_rmse
 from src.direct_model.interferometer import simulate_interferogram
 from src.interface.configuration import load_config
-from src.inverse_model.protocols import inversion_protocol_factory
 
 
 def main():
     config = load_config()
     db = config.database()
 
+    reports_folder = config.directory_paths.reports
+
     experiment_id = 0
-    experiment_params = db.experiments[experiment_id]
+    experiment_config = db.experiments[experiment_id]
+    pprint(dict(experiment_config))
+    experiment_dir = reports_folder / f"experiment_{experiment_id}" / "reconstruction"
 
-    # EXTRA: For saving the full table of RMSEs in the paper
-    nb_dss = len(experiment_params.dataset_ids)
-    nb_ifms = len(experiment_params.interferometer_ids)
-    nb_nls = len(experiment_params.noise_level_indices)
-    nb_ips = len(experiment_params.inversion_protocol_ids)
-
-    # EXTRA: For saving the full table of RMSEs in the paper
-    reports_dir = config.directory_paths.reports
-    experiment_dir = reports_dir / f"experiment_{experiment_id}"
-    if not experiment_dir.exists():
-        experiment_dir.mkdir(parents=True, exist_ok=True)
-    full_table = np.zeros(shape=(nb_dss * nb_ips, nb_ifms * nb_nls * 2))
-
-    for i_ds, ds_id in enumerate(experiment_params.dataset_ids):
+    for ds_id in experiment_config.dataset_ids:
         spectra_ref = db.dataset_spectrum(ds_id=ds_id)
         print(f"Dataset: {db.datasets[ds_id].title.upper()}")
-
-        # EXTRA: For saving the results: Lambda_min, RMSE, Reconstructed Spectra
         dataset_dir = experiment_dir / f"invert_{db.datasets[ds_id].title}"
-        if not dataset_dir.exists():
-            dataset_dir.mkdir(parents=True, exist_ok=True)
-        lambda_min_table = np.zeros(shape=(nb_ifms, nb_ips, nb_nls))
-        rmse_table = np.zeros(shape=(nb_ifms, nb_ips, nb_nls*2))
-        spectra_rec_table = np.zeros(shape=(nb_ifms, nb_ips, nb_nls, *spectra_ref.data.shape))
 
-        for i_ifm, ifm_id in enumerate(experiment_params.interferometer_ids):
+        for ifm_id in experiment_config.interferometer_ids:
             interferometer = db.interferometer(ifm_id=ifm_id)
             print(f"\tInterferometer: {db.interferometers[ifm_id].title.upper()}")
+            interferometer_dir = dataset_dir / f"{db.interferometers[ifm_id].title}"
 
-            # Generate the transmittance response (aka Transfer Matrix)
             transfer_matrix = interferometer.transmittance_response(wavenumbers=spectra_ref.wavenumbers)
+            interferograms_ref = simulate_interferogram(transmittance_response=transfer_matrix, spectrum=spectra_ref)
 
-            # Simulate the interferograms (aka the data / observation / acquisition)
-            interferograms = simulate_interferogram(transmittance_response=transfer_matrix, spectrum=spectra_ref)
-
-            # Preprocessing, normalizing the transfer matrix (linear operator) and the data
+            interferograms_ref = interferograms_ref.rescale(new_max=1, axis=-2)
             transfer_matrix = transfer_matrix.rescale(new_max=1, axis=None)
-            interferograms = interferograms.rescale(new_max=1, axis=-2)
 
-            for i_nl, nl_idx in enumerate(experiment_params.noise_level_indices):
+            for nl_idx in experiment_config.noise_level_indices:
                 snr_db = db.noise_levels[nl_idx]
                 print(f"\t\tSNR: {snr_db} dB")
+                noise_level_dir = interferometer_dir / f"{snr_db}_db"
 
-                # Add noise to the interferogram
                 np.random.seed(0)
-                interferograms_noisy = interferograms.add_noise(snr_db=snr_db)
+                interferograms_noisy = interferograms_ref.add_noise(snr_db=snr_db)
 
-                for i_ip, ip_id in enumerate(experiment_params.inversion_protocol_ids):
-                    ip_schema = db.inversion_protocols[ip_id]
-                    lambdaas = ip_schema.lambdaas_schema.as_array()
-                    print(f"\t\t\tSNR: {ip_schema.title.upper()}")
+                for ip_id in experiment_config.inversion_protocol_ids:
+                    lambdaas = db.inversion_protocol_lambdaas(inv_protocol_id=ip_id)
+                    print(f"\t\tInversion Protocol: {db.inversion_protocols[ip_id].title.upper()}")
+                    inverter_dir = noise_level_dir / f"{db.inversion_protocols[ip_id].title}"
 
-                    # EXTRA: For selecting the Reconstructed Spectra our of all lambdaas with the minimum RMSE
-                    spectra_rec_lambdaas = np.zeros(shape=(lambdaas.size, *spectra_ref.data.shape))
+                    spectra_rec_all = np.zeros(shape=(lambdaas.size, *spectra_ref.data.shape))
 
                     for i_lmd, lambdaa in enumerate(lambdaas):
-                        ip_kwargs = ip_schema.parameters(lambdaa=lambdaa)
-                        inverter = inversion_protocol_factory(option=ip_schema.type, parameters=ip_kwargs)
+                        inverter = db.inversion_protocol(inv_protocol_id=ip_id, lambdaa=lambdaa)
                         spectra_rec = inverter.reconstruct_spectrum(
                             interferogram=interferograms_noisy, transmittance_response=transfer_matrix
                         )
+                        spectra_rec_all[i_lmd] = spectra_rec.data
 
-                        # EXTRA: Store the spectral data array with respect to lambdaa
-                        spectra_rec_lambdaas[i_lmd] = spectra_rec.data
-
-                    # EXTRA: Store the results with minimum RMSE
                     rmse_lambdaas = calculate_rmse(
-                        array=spectra_rec_lambdaas,
-                        reference=spectra_ref.data[np.newaxis],
+                        array=spectra_rec_all,
+                        reference=spectra_ref.data,
                         is_match_axis=-2,
                         is_match_stats=True,
                         is_rescale_reference=True,
                     )
-                    argmin = np.argmin(rmse_lambdaas)
-                    lambda_min_table[i_ifm, i_ip, i_nl] = lambdaas[argmin]
-                    rmse_table[i_ifm, i_ip, i_nl] = rmse_lambdaas[argmin]
-                    spectra_rec_table[i_ifm, i_ip, i_nl] = spectra_rec_lambdaas[argmin]
-                    print(f"\t\t\t\tLambda: {lambda_min_table[i_ifm, i_ip, i_nl]:.3f}")
-                    print(f"\t\t\t\tRMSE:   {rmse_table[i_ifm, i_ip, i_nl]:.3f}")
+                    argmin_rmse = np.argmin(rmse_lambdaas)
+                    spectra_rec_best = spectra_rec_all[argmin_rmse]
 
-                    # EXTRA: Store the results with minimum RMSE in the full table for the paper
-                    full_table[i_ip + nb_ips*i_ds, 2*i_nl + 2*nb_nls*i_ifm] = lambdaas[argmin]
-                    full_table[i_ip + nb_ips*i_ds, 2*i_nl + 2*nb_nls*i_ifm + 1] = rmse_lambdaas[argmin]
+                    # Save the best reconstruction, per inv_protocol per dataset
+                    # TODO: Save file_list (serialize)
+                    if not inverter_dir.exists():
+                        inverter_dir.mkdir(parents=True, exist_ok=True)
+                    np.save(file=inverter_dir / "spectra_rec_best", arr=spectra_rec_best)
+                    np.save(file=inverter_dir / "argmin_rmse", arr=argmin_rmse)
 
-        # EXTRA: Save the results with minimum RMSE
-        file_name_list = ["lambdaa_min", "rmse", "rec_spectra"]
-        table_list = [lambda_min_table, rmse_table, spectra_rec_table]
-        for file_name, table in zip(file_name_list, table_list):
-            np.save(file=dataset_dir / file_name, arr=table)
-
-    # EXTRA: Save the full table for the paper
-    file_name = "full_table"
-    np.save(file=experiment_dir / file_name, arr=full_table)
-    pprint(full_table)
+                # Save the noisy interferograms
+                if not noise_level_dir.exists():
+                    noise_level_dir.mkdir(parents=True, exist_ok=True)
+                np.save(file=noise_level_dir / "interferograms_noisy", arr=interferograms_noisy.data)
 
 
 if __name__ == "__main__":
