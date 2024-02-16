59070a76b213bb481e93be952c08e5fab0c0ec1d
Mohamad Jouni <33843331+mhmdjouni@users.noreply.github.com>
2024-02-16 19:50:06 +0100

    Real experiments full metrics table

--- src/demo/experiments_paper/monochromatic/utils.py
+++ src/demo/experiments_paper/monochromatic/utils.py
@@ -7,6 +7,9 @@ import numpy as np
 from src.common_utils.custom_vars import Wvn, Acq
 from src.common_utils.interferogram import Interferogram
 from src.common_utils.light_wave import Spectrum
+from src.demo.experiments_paper.snr.utils import experiment_dir_convention
+from src.interface.configuration import load_config
+from src.outputs.serialize import numpy_load_list
 
 
 def print_metrics(nb_tabs, category, best_idx, lambdaa, rmse_full, rmse_diagonal, rmcw, wavenumbers_size):
@@ -133,3 +136,80 @@ def crop_interferogram_acquisitions(
     mask = (new_range[0] <= central_wavenumbers) & (central_wavenumbers <= new_range[-1])
     new_data = interferogram.data[:, mask]
     return replace(interferogram, data=new_data)
+
+
+def experiment_subdir_convention(
+        dataset_id: int,
+        characterization_id: int = -1,
+        noise_level_index: int = -1,
+        inversion_protocol_id: int = -1,
+):
+    config = load_config()
+    db = config.database()
+    subdir = f"invert_{db.datasets[dataset_id].title}"
+    if characterization_id >= 0:
+        subdir = f"{subdir}/{db.characterizations[characterization_id].title}"
+    if noise_level_index >= 0:
+        subdir = f"{subdir}/{int(db.noise_levels[noise_level_index])}_db"
+    if inversion_protocol_id >= 0:
+        subdir = f"{subdir}/{db.inversion_protocols[inversion_protocol_id].title}"
+    return subdir
+
+
+def metrics_full_table(experiment_id: int):
+    config = load_config()
+    db = config.database()
+    experiment_config = db.experiments[experiment_id]
+
+    nb_dss = len(experiment_config.dataset_ids)
+    nb_ifms = len(experiment_config.interferometer_ids)
+    nb_nls = len(experiment_config.noise_level_indices)
+    nb_ips = len(experiment_config.inversion_protocol_ids)
+    full_table = np.zeros(shape=(nb_dss * nb_ips, nb_ifms * nb_nls * 4))
+
+    index = []
+    for _ in experiment_config.dataset_ids:
+        for ip_id in experiment_config.inversion_protocol_ids:
+            gls_str = "& \\gls{" + f"{db.inversion_protocols[ip_id].title.lower()}" + "}"
+            index.append(gls_str)
+
+    header = []
+    for _ in experiment_config.interferometer_ids:
+        for _ in experiment_config.noise_level_indices:
+            header.append("lambda")
+            header.append("rmse diag")
+            header.append("rmse full")
+            header.append("rmcw")
+
+    for i_ds, ds_id in enumerate(experiment_config.dataset_ids):
+        for i_ifm, ifm_id in enumerate(experiment_config.interferometer_ids):
+            for i_nl, nl_idx in enumerate(experiment_config.noise_level_indices):
+                for i_ip, ip_id in enumerate(experiment_config.inversion_protocol_ids):
+                    lambdaas = db.inversion_protocol_lambdaas(inv_protocol_id=ip_id)
+                    metrics_dir = experiment_dir_convention(dir_type="metrics", experiment_id=experiment_id)
+                    inverter_subdir = experiment_subdir_convention(
+                        dataset_id=ds_id,
+                        characterization_id=ifm_id,
+                        noise_level_index=nl_idx,
+                        inversion_protocol_id=ip_id,
+                    )
+
+                    rmse_diagonal, rmse_full, rmcw = numpy_load_list(
+                        filenames=["rmse_diagonal.npy", "rmse_full.npy", "rmcw.npy"],
+                        directory=metrics_dir,
+                        subdirectory=inverter_subdir,
+                    )
+                    argmin = np.argmin(rmse_diagonal)
+                    lambdaa_min = lambdaas[argmin]
+                    rmse_diagonal_min = rmse_diagonal[argmin]
+                    rmse_full_min = rmse_full[argmin]
+                    rmcw_min = rmcw[argmin]
+                    if lambdaa_min == 0:
+                        lambdaa_min = np.nan
+
+                    full_table[i_ip + nb_ips*i_ds, 2*i_nl + 2*nb_nls*i_ifm] = lambdaa_min
+                    full_table[i_ip + nb_ips*i_ds, 2*i_nl + 2*nb_nls*i_ifm + 1] = rmse_diagonal_min
+                    full_table[i_ip + nb_ips*i_ds, 2*i_nl + 2*nb_nls*i_ifm + 2] = rmse_full_min
+                    full_table[i_ip + nb_ips*i_ds, 2*i_nl + 2*nb_nls*i_ifm + 3] = rmcw_min
+
+    return full_table, header, index
