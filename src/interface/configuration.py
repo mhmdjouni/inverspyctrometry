95dde2c96dd2a415211900e0d8b399ce8480d8a4
Mohamad Jouni <33843331+mhmdjouni@users.noreply.github.com>
2024-04-08 03:48:02 +0200

    Resolve configuration paths automatically

--- src/interface/configuration.py
+++ src/interface/configuration.py
@@ -1,19 +1,54 @@
+import functools
 import json
 from pathlib import Path
 
 from omegaconf import OmegaConf
-from pydantic import BaseModel, DirectoryPath, FilePath
+from pydantic import BaseModel, DirectoryPath, FilePath, model_validator, field_validator, Field
 
 from src.database.database import DatabaseSchema
 
 
+def resolve_path(path: str) -> str:
+    path_dict = OmegaConf.create({"__root__": path})
+    path_fix = OmegaConf.to_container(path_dict, resolve=True)
+    return path_fix["__root__"]
+
+
 class DirectoryPathSchema(BaseModel):
-    project: DirectoryPath
-    data: DirectoryPath
-    datasets: DirectoryPath
-    notebooks: DirectoryPath
-    reports: DirectoryPath
-    src: DirectoryPath
+    data: DirectoryPath = Field(title="DATA_DIR")
+    datasets: DirectoryPath = Field(title="DATASETS_DIR")
+    notebooks: DirectoryPath = Field(title="NOTEBOOKS_DIR")
+    reports: DirectoryPath = Field(title="REPORTS_DIR")
+    src: DirectoryPath = Field(title="SRC_DIR")
+
+    @model_validator(mode="before")
+    def register_project_dir(cls, values):
+        """Registers the PROJECT_DIR resolver using OmegaConf"""
+        resolver_func = functools.partial(lambda x: x, x=cls.__project_dir())
+        OmegaConf.register_new_resolver(name="PROJECT_DIR", resolver=resolver_func, replace=True)
+        return values
+
+    @field_validator(__field="*", mode="before")
+    def resolve_dir(cls, directory: DirectoryPath) -> DirectoryPath:
+        """Takes a field and completes / resolves it based on what was registered in the OmegaConf resolver before"""
+        directory_resolved = resolve_path(path=directory)
+        return directory_resolved
+
+    @model_validator(mode="after")
+    def register_attributes_dirs(cls, values):
+        for field, value in cls.model_fields.items():
+            directory = getattr(values, field).as_posix()
+            resolver_func = functools.partial(lambda x: x, x=directory)
+            OmegaConf.register_new_resolver(name=value.title, resolver=resolver_func, replace=True)
+        return values
+
+    @property
+    def project(self):
+        return self.__project_dir()
+
+    @staticmethod
+    def __project_dir() -> DirectoryPath:
+        return Path(__file__).resolve().parents[2]
 
 
 class DatabasePathSchema(BaseModel):
@@ -23,6 +58,12 @@ class DatabasePathSchema(BaseModel):
     inversion_protocols: FilePath
     noise_levels: FilePath
     experiments: FilePath
+
+    @field_validator(__field="*", mode="before")
+    def resolve_filepaths(cls, filepath: FilePath) -> FilePath:
+        """Takes a field and completes / resolves it based on what was registered in the OmegaConf resolver before"""
+        filepath_resolved = resolve_path(path=filepath)
+        return filepath_resolved
 
     def open(self) -> DatabaseSchema:
         characterizations_dict = self._load_dict_from_json(path=self.characterizations)
@@ -44,9 +85,9 @@ class DatabasePathSchema(BaseModel):
     def _load_dict_from_json(path: Path) -> dict:
         with open(path) as json_opened:
             database_dict = json.load(json_opened)
-        database_obj = OmegaConf.create(database_dict)
-        root_dict = OmegaConf.to_container(database_obj, resolve=True)
-        return root_dict
+        # database_obj = OmegaConf.create(database_dict)
+        # database_dict = OmegaConf.to_container(database_obj, resolve=True)
+        return database_dict
 
 
 class ConfigSchema(BaseModel):
@@ -63,3 +104,13 @@ def load_config(config_sub_path: Path | str = "data/database/config.json") -> Co
     with open(config_path) as config_file:
         config_dict = json.load(fp=config_file)
     return ConfigSchema(**config_dict)
+
+
+def main():
+    config = load_config()
+    database = config.database()
+    print(database)
+
+
+if __name__ == "__main__":
+    main()
