ccb1f3f773e7cc66e73710dccb2e975e025d9161
Mohamad Jouni <33843331+mhmdjouni@users.noreply.github.com>
2024-04-15 21:00:03 +0200

    reproduce haar paper's parameters test

---
+++ src/common_utils/function_generator.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+from dataclasses import dataclass, replace
+
+import numpy as np
+
+from src.common_utils.custom_vars import Acq, Wvn
+
+
+@dataclass
+class GaussianGenerator:
+    coefficients: np.ndarray[tuple[int, Acq], np.dtype[np.float_]]
+    means: np.ndarray[tuple[int, Acq], np.dtype[np.float_]]
+    stds: np.ndarray[tuple[int, Acq], np.dtype[np.float_]]
+
+    def min_max_parameters(
+            self,
+            ref_min,
+            ref_max,
+            new_min,
+            new_max,
+    ):
+        means = new_min + (self.means - ref_min) / ref_max * (new_max - new_min)
+        stds = self.stds / ref_max * (new_max - new_min)
+        return replace(self, means=means, stds=stds)
+
+    def generate(
+            self,
+            variable: np.ndarray[tuple[Wvn], np.dtype[np.float_]],
+    ) -> np.ndarray[tuple[Wvn, Acq], np.dtype[np.float_]]:
+        variable_centered = (variable[None, None, :] - self.means[:, :, None]) / self.stds[:, :, None]
+        gaussian_funcs = np.exp(-variable_centered**2)
+        data = np.sum(self.coefficients[:, :, None] * gaussian_funcs, axis=0).T
+        return data
--- src/demo/paper/haar_reconstruction.py
+++ src/demo/paper/haar_reconstruction.py
@@ -1,42 +1,13 @@
-from dataclasses import dataclass, replace
-
 import numpy as np
 from matplotlib import pyplot as plt
 
-from src.common_utils.custom_vars import Wvn, Acq
 from src.common_utils.light_wave import Spectrum
+from src.common_utils.function_generator import GaussianGenerator
 from src.direct_model.interferometer import FabryPerotInterferometer
 from src.inverse_model.inverspectrometer import FabryPerotInverSpectrometerHaar
 
 
-@dataclass
-class GaussianGenerator:
-    coefficients: np.ndarray[tuple[int, Acq], np.dtype[np.float_]]
-    means: np.ndarray[tuple[int, Acq], np.dtype[np.float_]]
-    stds: np.ndarray[tuple[int, Acq], np.dtype[np.float_]]
-
-    def min_max_parameters(
-            self,
-            ref_min,
-            ref_max,
-            new_min,
-            new_max,
-    ):
-        means = new_min + (self.means - ref_min) / ref_max * (new_max - new_min)
-        stds = self.stds / ref_max * (new_max - new_min)
-        return replace(self, means=means, stds=stds)
-
-    def generate(
-            self,
-            variable: np.ndarray[tuple[Wvn], np.dtype[np.float_]],
-    ) -> np.ndarray[tuple[Wvn, Acq], np.dtype[np.float_]]:
-        variable_centered = (variable[None, None, :] - self.means[:, :, None]) / self.stds[:, :, None]
-        gaussian_funcs = np.exp(-variable_centered**2)
-        data = np.sum(self.coefficients[:, :, None] * gaussian_funcs, axis=0).T
-        return data
-
-
-def main():
+def my_test():
     wn_max_target = 1 / 350 * 1000  # um
     opd_step = 1 / (2 * wn_max_target)
     opd_num = 319
@@ -95,6 +66,84 @@ def main():
     spectrum_ref.visualize(axs=axs[0, 0], acq_ind=acq_ind, color="C0")
     spectrum_rec_eq.visualize(axs=axs[0, 0], acq_ind=acq_ind, linestyle="--", color="C1")
     plt.show()
+
+
+def paper_test(
+        gauss_coeffs,
+        gauss_means,
+        gauss_stds,
+        reflectance,
+        opd_step: float,
+        opd_num: int,
+        wn_min: float,
+        wn_max: float,
+):
+    opds = opd_step * np.arange(opd_num) + opd_step
+
+    wn_step = 1 / (2 * opds.max())
+    wn_max_dct = 1 / (2 * opd_step)
+    wn_num = int(opd_num * (wn_max - wn_min) / wn_max_dct)
+    wavenumbers = wn_min + wn_step * np.arange(wn_num)  # um-1
+    gaussian_gen = GaussianGenerator(
+        coefficients=gauss_coeffs[:, None],
+        means=gauss_means[:, None],
+        stds=gauss_stds[:, None],
+    )
+    spectrum_data = gaussian_gen.generate(variable=wavenumbers)
+    spectrum_ref = Spectrum(data=spectrum_data, wavenumbers=wavenumbers)
+
+    transmittance = np.array([1.])  # The values in the paper seem to be normalized by the transmittance
+
+    fp_0 = FabryPerotInterferometer(
+        transmittance_coefficients=transmittance,
+        opds=opds,
+        phase_shift=np.array([0]),
+        reflectance_coefficients=reflectance,
+        order=0,
+    )
+    interferogram = fp_0.acquire_interferogram(spectrum=spectrum_ref)
+
+    order = 20
+    fp_haar = FabryPerotInverSpectrometerHaar(
+        transmittance=transmittance,
+        wavenumbers=wavenumbers,
+        reflectance=reflectance,
+        order=order,
+        is_mean_center=True,
+    )
+
+    coefficients = fp_haar.kernel_fourier_coefficients()
+    spectrum_rec = fp_haar.reconstruct_spectrum(interferogram=interferogram)
+
+    print(np.around(coefficients, decimals=3))
+    print()
+
+    acq_ind = 0
+
+    fig, axs = plt.subplots(1, 3, squeeze=False)
+
+    spectrum_ref.visualize(axs=axs[0, 0], acq_ind=acq_ind)
+
+    interferogram.visualize(axs=axs[0, 1], acq_ind=acq_ind)
+
+    spectrum_rec_eq, spectrum_ref = spectrum_rec.match_stats(reference=spectrum_ref, axis=-2)
+    spectrum_ref.visualize(axs=axs[0, 2], acq_ind=acq_ind, color="C0")
+    spectrum_rec_eq.visualize(axs=axs[0, 2], acq_ind=acq_ind, linestyle="--", color="C1")
+
+    plt.show()
+
+
+def main():
+    paper_test(
+        gauss_coeffs=np.array([1., 0.9, 0.75]),
+        gauss_means=np.array([2000, 4250, 6500]),  # cm
+        gauss_stds=np.array([300, 1125, 400]),  # cm
+        reflectance=np.array([0.7]),
+        opd_step=100*1e-7,  # cm
+        opd_num=2048,
+        wn_min=1000.,  # cm
+        wn_max=8000.,  # cm
+    )
 
 
 if __name__ == "__main__":
